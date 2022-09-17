b1822689adca03f8f21608273c18b796ff7987f1
sajjad <rezai_sajjad@yahoo.com>
2022-09-18 00:41:05 +0430

    main edited

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 50; i++) {
-            BirthdayCalculator bc = new BirthdayCalculator(23);
-            for (int j = 0; j < 1000; j++) {
+        BirthdayCalculator bc = new BirthdayCalculator(23);
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 1000000; j++) {
                 bc.isThereSameBirthday();
             }
             bc.printResult();
