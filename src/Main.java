61eef9544e4a2bbf385ef810249b2633fa6dc100
sajjad <rezai_sajjad@yahoo.com>
2022-09-18 00:10:34 +0430

    calculate 1000 times

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        BirthdayCalculator bc = new BirthdayCalculator(23);
+        for (int i = 0; i < 1000; i++) {
+            bc.isThereSameBirthday();
+        }
+        bc.printResult();
     }
 }
